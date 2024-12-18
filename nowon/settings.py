9815c4cbf8e3f89e30cee02a826616210290d92b
Felipe Nasciemnto <Feliph08@hotmail.com>
2024-12-17 21:41:02 -0300

    finalizado?

--- nowon/settings.py
+++ nowon/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-#cm8^!rb3vgrqcw!q5d565^g(9@34yq_ccoxdv7p1i6sr*7)av
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['feliphfl.pythonanywhere.com', 'thefeliph.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['feliphfl.pythonanywhere.com', 'localhost', '127.0.0.1']
+
 
 
 
